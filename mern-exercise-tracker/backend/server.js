4d3d55e4d2cbb08bdfef25f47d3a4d7e3f2bebc0
khoatran162000 <khoatranchemistry@gmail.com>
2022-05-14 15:43:42 +0700

    first commit

---
+++ mern-exercise-tracker/backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);				//localhost:5000/exercises/
+app.use('/users', usersRouter);								//localhost:5000/users/
+
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
